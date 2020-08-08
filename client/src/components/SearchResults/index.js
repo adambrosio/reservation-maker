de10243041c98e108a387d5fc242f0c2b3812d05
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-08-08 11:29:23 -0400

    search results and results list components created

--- client/src/components/SearchResults/index.js
+++ client/src/components/SearchResults/index.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import Navbar from "../Navbar";
+import ResultsList from "../ResultsList";
 import "./style.css";
 
-function SearchResults(props) {
-  return (
-    <ul className="list-group search-results">
-      {props.results.map(result => (
-        <li key={result} className="list-group-item">
-          <img alt="Dog" src={result} className="img-fluid" />
-        </li>
-      ))}
-    </ul>
-  );
+class SearchResults extends Component {
+  state = {
+    search: "",
+    results: []
+  };
+
+  businessSearch = query => {
+
+  }
+
+  handleInputChange = event => {
+    // any business that matches the search input value
+    const business = event.target.business;
+    const value = event.target.value;
+    this.setState({
+      [business]: value
+    });
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="search-results">{this.state.search}</div>
+        <Navbar
+          search={this.state.search}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
+        <ResultsList results={this.state.results} />
+      </div>
+    );
+  }
 }
 
 export default SearchResults;
