83d49e4396c3c2577e72baccb65f420afa419d59
Matt Fava <matt.b.fava@gmail.com>
2020-08-11 18:40:46 -0400

    home page navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,6 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import Form from "./components/Form";
-import Button from "./components/Button";
-import { TopBar, TopBarRight, TopBarLeft } from 'react-foundation';
-import { Menu, MenuItem, MenuText } from 'react-foundation';
-import Login from "./components/loginPage";
-import { Router } from "react-router-dom";
 import 'foundation-sites/dist/css/foundation.min.css';
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"
 import Home from "./pages/home";
-import Jumbotron from "./components/Jumbotron"
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
