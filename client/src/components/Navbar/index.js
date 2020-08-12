12a0eea85bf73e6392b859fd90c6036c241d2531
Reiner Bonsol <reinerbonsol93@gmail.com>
2020-08-12 08:18:12 -0400

    added new components to create a login page and signup page with working buttons that will route you to the appropriate pages

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,36 +1,39 @@
 import React from "react";
 import Foundation from 'foundation-sites';
+import { Link } from "react-router-dom";
 import { TopBar, TopBarRight, TopBarLeft, Sizes } from 'react-foundation';
-import { Menu, MenuItem, MenuText, Button, MenuDropdown, MenuDropdownContent,MenuDropdownTitle} from 'react-foundation';
+import { Menu, MenuItem, MenuText, Button, MenuDropdown, MenuDropdownContent, MenuDropdownTitle } from 'react-foundation';
 import 'foundation-sites/dist/css/foundation.min.css';
 import "./style.css"
 
 
 function Navbar(props) {
 
-    return (
-<TopBar className='myTopBar'>
-        <TopBarLeft className='my-top-bar-right'>
-          <Menu className='menuLeft'>
+  return (
+    <TopBar className='myTopBar'>
+      <TopBarLeft className='my-top-bar-right'>
+        <Menu className='menuLeft'>
           <MenuText>Reservation Maker</MenuText>
-            <MenuItem isActive><a>Home</a></MenuItem>
-            <label className = "search">
-            <textarea placeholder = "Search for Business..."></textarea>
+          <Link to="/home">
+            Home
+          </Link>
+          <label className="search">
+            <textarea placeholder="Search for Business..."></textarea>
           </label>
-          </Menu>
-          
-        </TopBarLeft>
-
-        <TopBarRight>
-                <Menu className='menuRight'>
-        <MenuItem isActive><a>{props.accountName}</a></MenuItem>
         </Menu>
-        </TopBarRight>
 
+      </TopBarLeft>
 
+      <TopBarRight>
+        <Menu className='menuRight'>
+          <Link to="/signup">
+            Signup/Login
+          </Link>
+          <MenuItem isActive><a>{props.accountName}</a></MenuItem>
+        </Menu>
+      </TopBarRight>
+    </TopBar>
 
-      </TopBar>
-       
-    )
+  )
 }
 export default Navbar;
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import SignupForm from "../components/SignupForm";
+import "../components/signupTest.css";
+
+const emailRegex = RegExp(
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+);
+
+const formValid = ({ formErrors, ...rest }) => {
+    let valid = true;
+
+    // validate form errors being empty
+    Object.values(formErrors).forEach(val => {
+        val.length > 0 && (valid = false);
+    });
+
+    // validate the form was filled out
+    Object.values(rest).forEach(val => {
+        val === null && (valid = false);
+    });
+
+    return valid;
+};
+
+class Signup extends Component {
+    state = {
+        firstName: null,
+        lastName: null,
+        email: null,
+        password: null,
+        formErrors: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: ""
+        }
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        if (formValid(this.state)) {
+            console.log(`
+            --SUBMITTING--
+            First Name: ${this.state.firstName}
+            Last Name: ${this.state.lastName}
+            Email: ${this.state.email}
+            Password: ${this.state.password}
+        `);
+        } else {
+            console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+        }
+    };
+
+    handleChange = e => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        let formErrors = { ...this.state.formErrors };
+
+        switch (name) {
+            case "firstName":
+                formErrors.firstName =
+                    value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+            case "lastName":
+                formErrors.lastName =
+                    value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+            case "email":
+                formErrors.email = emailRegex.test(value)
+                    ? ""
+                    : "invalid email address";
+                break;
+            case "password":
+                formErrors.password =
+                    value.length < 6 ? "minimum 6 characaters required" : "";
+                break;
+            default:
+                break;
+        }
+
+        this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+    };
+
+    render() {
+        return (
+            <div>
+                <SignupForm 
+                    handleChange={this.handleChange} 
+                    handleSubmit={this.handleSubmit}
+                    firstName={this.state.formErrors.firstName}
+                    lastName={this.state.formErrors.lastName}
+                    email={this.state.formErrors.email}
+                    password={this.state.formErrors.password}
+                />
+            </div>
+        );
+    }
+}
+export default Signup;
