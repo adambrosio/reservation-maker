6b443b8f637a679e6cd0cf06cbf3886777517a25
Matt <mbatko0914@gmail.com>
2020-08-12 08:04:52 -0400

    get user profile info & reservations

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import 'foundation-sites/dist/css/foundation.min.css';
 import Home from "./pages/home";
+import './App.css';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Profile from './pages/profile'
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-     <Home accountName = "MattF"></Home>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home accountName = "MattF"></Home>
+          </Route>
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+          </Switch>
+     </Router>
     )
   }
 }
